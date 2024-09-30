cb3e990e5132d21161bdaa475053bfa744cf57ca
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-09-30 15:37:34 -0300

    Criando decorator de autenticação

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,18 +1,16 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { UserPayload } from 'src/auth/jwt.strategy'
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(
-    private jwt: JwtService,
-    private prisma: PrismaService,
-  ) {}
+  constructor() {}
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
     return 'ok'
   }
 }
