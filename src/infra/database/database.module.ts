32a37cef0b4a9cc601cde7801e3bba00e5464cb6
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-10-08 12:01:29 -0300

    Refatorando controller de Autenticação

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -7,6 +7,8 @@ import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-re
 import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachment-repository'
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
   providers: [
@@ -14,6 +16,10 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
     {
       provide: QuestionsRepository,
       useClass: PrismaQuestionsRepository,
+    },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
     },
     PrismaAnswersRepository,
     PrismaAnswerCommentsRepository,
@@ -23,8 +29,9 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
   ],
   exports: [
     PrismaService,
-    PrismaAnswersRepository,
     QuestionsRepository,
+    StudentsRepository,
+    PrismaAnswersRepository,
     PrismaAnswerCommentsRepository,
     PrismaQuestionCommentsRepository,
     PrismaAnswerAttachmentsRepository,
